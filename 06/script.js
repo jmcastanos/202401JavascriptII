75a4eb5fb5104722697337153d4f9c4aa42424c2
Juan Miguel Castanos <jmcastanos@webinver.com>
2024-02-13 19:18:54 -0800

    localstorage

--- 06/script.js
+++ 06/script.js
@@ -1,20 +1,31 @@
+let tasks = window.localStorage.getItem("pendientes");
+let taskList = document.getElementById("task-list");
+taskList.innerHTML = tasks;
+
 function guardarPendiente(){
     //
 
-    let task = document.getElementById("new-task")
+    let task = document.getElementById("new-task");
+    let taskList = document.getElementById("task-list");
     if(task.value == ""){
-        alert("No puedes guardar una tarea vacía")
-        return
+        alert("No puedes guardar una tarea vacía");
+        return;
     }
 
+    let newTask = document.createElement("li");
+    newTask.textContent = task.value;
+    taskList.appendChild(newTask);
+
     task.value = ""
-
-    
-
     alert("Pendiente guardado")
 
-
+    window.localStorage.setItem("pendientes", taskList.innerHTML);
 }
+
+document.getElementById("formtodo").addEventListener("submit", function(e){
+    e.preventDefault();
+    guardarPendiente()
+});
 
 document.getElementById("formtodo").addEventListener("submit", function(e){
     e.preventDefault();
